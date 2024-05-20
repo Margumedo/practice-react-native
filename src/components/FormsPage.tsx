b5bc293fe37702706c845c3bd6aafd72af7131fc
Maicol Argumedo <margumedo.sm@gmail.com>
2024-05-19 23:06:51 -0400

    FormsPage builting

--- src/components/FormsPage.tsx
+++ src/components/FormsPage.tsx
@@ -1,14 +1,26 @@
+import { useForm } from "react-hook-form"
 
+type FormInputs = {
+    email: string;
+    password: string;
+}
 
 export const FormsPage = () => {
+
+    const { register } = useForm<FormInputs>({
+        defaultValues: {
+            email: 'maicolino@google.com',
+            password: 'test1234'
+        }
+    });
 
     return (
         <>
             <h3>Formulario</h3>
             <form>
                 <div style={{ display: 'flex', flexDirection: 'column' }} >
-                    <input type="text" placeholder="Email" />
-                    <input type="text" placeholder="Password" />
+                    <input type="text" placeholder="Email" {...register('email')} />
+                    <input type="text" placeholder="Password"   {...register('password')} />
                     <button type="submit" >Ingresar</button>
                 </div>
             </form>
